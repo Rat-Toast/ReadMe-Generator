61167efaced6514a98e35be22b5bb87f81642999
paloma wells <palomawells@palomas-MacBook.home>
2023-11-17 10:31:17 -0600

    added input fields into index.js

--- index.js
+++ index.js
@@ -22,3 +22,84 @@ function writeToFile(fileName, data) {
   );
 }
 
+function generateLicense(license) {
+    if (license === "MIT") {
+      return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+    } else if (license === "Apache 2.0 License") {
+      return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+    } else if (license === "IBM Public License Version 1.0") {
+      return `[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)`;
+    } else if (license === "Mozilla Public License 2.0") {
+      return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`;
+    } else {
+      return `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`;
+    }
+  }
+  
+  function init() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          message: "What is your name?",
+          name: "name",
+        },
+        {
+          type: "input",
+          message: "What is the title of your project?",
+          name: "title",
+        },
+        {
+          type: "input",
+          message: "Add the description of your project:",
+          name: "description",
+        },
+        {
+          type: "input",
+          message: "Add the installation instructions of your project:",
+          name: "installation",
+        },
+        {
+          type: "input",
+          message: "Add the usage information of your project:",
+          name: "usage",
+        },
+        {
+          type: "input",
+          message: "Add the contribution guidelines of your project:",
+          name: "contribution",
+        },
+        {
+          type: "input",
+          message: "Add the test instructions of your project:",
+          name: "test",
+        },
+        {
+          type: "list",
+          message: "Select the type of license you would like for your project:",
+          choices: [
+            "MIT",
+            "Apache 2.0 License",
+            "IBM Public License Version 1.0",
+            "Mozilla Public License 2.0",
+            "The Unlicense",
+          ],
+          name: "license",
+        },
+        {
+          type: "input",
+          message: "What is your GitHub handle?",
+          name: "github",
+        },
+        {
+          type: "input",
+          message: "What is your email?",
+          name: "email",
+        },
+      ])
+      .then((data) => {
+        writeToFile("sample-README.md", data);
+      });
+  }
+  
+  init();
